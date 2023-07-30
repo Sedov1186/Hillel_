f75169dc51e2db8dfec10f6f5d086a7347f77a5a
Дмитрий <agent1935@gmail.com>
2023-07-30 14:53:43 +0300

    Add File

--- Hillel_7 Функция подсчёта.py
+++ Hillel_7 Функция подсчёта.py
@@ -1,5 +1,4 @@
 import random
-
 random_list = [random.randint(1, 10) for _ in range(200)]
 count_dict = {num: sum(1 for x in random_list if x == num) for num in set(random_list)}
 get_plural_form = lambda num: "раз" if num == 1 else "раза"
