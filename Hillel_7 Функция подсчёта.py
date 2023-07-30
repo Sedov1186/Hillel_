062e2834daf25c575762b16d4fbaaa2c6507d197
Дмитрий <agent1935@gmail.com>
2023-07-30 14:52:16 +0300

    Add File

---
+++ Hillel_7 Функция подсчёта.py
@@ -0,0 +1,7 @@
+import random
+
+random_list = [random.randint(1, 10) for _ in range(200)]
+count_dict = {num: sum(1 for x in random_list if x == num) for num in set(random_list)}
+get_plural_form = lambda num: "раз" if num == 1 else "раза"
+for num, count in sorted(count_dict.items()):
+    print(f"Число {num} встречается в первоначальном списке {count} {get_plural_form(count)}")
